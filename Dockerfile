60f0a3559be51d2b7f477f998bf69471843805a3
ZhangYuan <zyct@vip.qq.com>
2018-11-09 14:54:50 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ADD root/ /root
 
 RUN cd /root/go-ipfs/ && sh install.sh
 
+RUN chmod 755 /root/entrypoint.sh
+
 EXPOSE 4001 5001 8080
 
 WORKDIR /root
--- dev.sh
+++ dev.sh
@@ -3,13 +3,21 @@ if [ $# != 1 ] ; then
     echo 'run as: sh dev.sh build or sh dev.sh run'
     exit
 fi
-nameTag="ipfs:v1"
+name="ipfs"
+tag="latest"
 case $1 in
     "build")
-        docker build -t $nameTag .
+        docker build -t $name:$tag .
     ;;
     "run")
-        docker run $nameTag
+        docker run --name $name  -p 4001:4001 -p 5001:5001 -p 8080:8080 $name:$tag &
+    ;;
+    "bash")
+        docker exec -it $name bash
+    ;;
+    "stop")
+        docker stop $name
+        docker rm $name
     ;;
     *) 
         echo 'not support action'
